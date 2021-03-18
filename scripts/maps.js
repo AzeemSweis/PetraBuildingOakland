aa54325843fe185bce3308a8fb75d6881db77413
Azeem Sweis <azeemsw@gmail.com>
2021-03-18 13:41:00 -0700

    add gallery images, relocate js to external file, add delay to load time

---
+++ scripts/maps.js
@@ -0,0 +1,19 @@
+function initMap() {
+  // The location of Uluru
+  const petraOakland = {
+    lat: 37.80688,
+    lng: -122.27177
+  };
+  // The map, centered at PBO
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: petraOakland,
+  });
+  // The marker, positioned at PBO
+  const marker = new google.maps.Marker({
+    position: petraOakland,
+    map: map,
+  });
+
+  $('body').show();
+}
